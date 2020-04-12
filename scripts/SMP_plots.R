1cd4e48a4ca8c15df698703e301ab122a9718ca7
Sasha D. Hafner <sasha.hafner@gmail.com>
2020-04-12 15:12:36 -0400

    Plot size tweaks

--- scripts/SMP_plots.R
+++ scripts/SMP_plots.R
@@ -14,4 +14,11 @@ ggplot(SMP, aes(time_d, SMP, colour = substrate, group = bottle_id)) +
   geom_line() + facet_wrap(~ paste0('Lab ', lab_id, ' test ', test_id)) +
   labs(x = 'Time (d)', y = expression('SMP'~(mL~g^'-1')), colour = 'Substrate')
 ggsave('../plots/SMP.png', height = 6, width = 6)
-ggsave('../plots/SMP_small.png', height = 6, width = 6, dpi = 96)
+
+# Small plot for README
+dd <- subset(SMP, lab_id == 19)
+ggplot(dd, aes(time_d, SMP, colour = substrate, group = bottle_id)) +
+  geom_line() + 
+  facet_wrap(~ paste0('Lab ', lab_id, ' test ', test_id), scale = 'free') +
+  labs(x = 'Time (d)', y = expression('SMP'~(mL~g^'-1')), colour = 'Substrate')
+ggsave('../plots/SMP_small.png', height = 3, width = 4, dpi = 96)
